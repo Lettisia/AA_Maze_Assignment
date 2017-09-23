ba50a090fa732d5671b73c455c042a285336d557
Lettisia <lettisiag@gmail.com>
2017-09-23 21:22:42 +1000

    Started Prim's Generator

--- src/mazeGenerator/ModifiedPrimsGenerator.java
+++ src/mazeGenerator/ModifiedPrimsGenerator.java
@@ -1,13 +1,43 @@
 package mazeGenerator;
 
-import maze.Maze;
+import java.util.ArrayList;
+import java.util.Random;
+
+import maze.*;
 
 public class ModifiedPrimsGenerator implements MazeGenerator {
-
+	private Random random = new Random();
+	
 	@Override
 	public void generateMaze(Maze maze) {
 		// TODO Auto-generated method stub
+		switch (maze.type) {
+		case Maze.NORMAL:
+			generateNormalMaze(maze);
+		case Maze.HEX:
+			generateHexMaze(maze);
+		case Maze.TUNNEL:
+			generateTunnelMaze(maze);
+		}
+	}
 
+	private void generateTunnelMaze(Maze maze) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void generateHexMaze(Maze maze) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void generateNormalMaze(Maze maze) {
+		// TODO Auto-generated method stub
+		ArrayList<Cell> z = new ArrayList<>();
+		z.add(maze.map[random.nextInt(maze.sizeR)][random.nextInt(maze.sizeC)]);
+		
+		
+		
 	} // end of generateMaze()
 
 } // end of class ModifiedPrimsGenerator
